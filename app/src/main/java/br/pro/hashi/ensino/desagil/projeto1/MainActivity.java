e765f644cce270987d6ddddf6d6f241268d94851
lucaslealvale <lucaslealvale01@gmail.com>
2019-09-30 15:19:14 -0300

    ajustes

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/MainActivity.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/MainActivity.java
@@ -15,10 +15,8 @@ public class MainActivity extends AppCompatActivity {
 
     private void startSMSActivity() {
 
-        // Constrói uma Intent que corresponde ao pedido de "iniciar Activity".
-        Intent intent = new Intent(this, SmsActivity.class);
 
-        // Inicia a Activity especificada na Intent.
+        Intent intent = new Intent(this, SmsActivity.class);
         startActivity(intent);
     }
     @Override
@@ -30,18 +28,11 @@ public class MainActivity extends AppCompatActivity {
 
         buttonExample.setOnClickListener((view) -> {
 
-            // Verifica se o aplicativo tem a permissão desejada.
             if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED) {
 
-                // Se tem, podemos iniciar a SMSActivity direto.
                 startSMSActivity();
             } else {
 
-                // Senão, precisamos pedir essa permissão.
-
-                // Cria um vetor de permissões a pedir. Como queremos
-                // uma só, parece um pouco feio, mas é bem conveniente
-                // quando queremos pedir várias permissões de uma vez.
                 String[] permissions = new String[]{
                         Manifest.permission.SEND_SMS,
                 };
@@ -50,16 +41,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
 
-        // Verifica se de fato é uma resposta ao pedido acima e se a
-        // resposta foi positiva. As respostas estão armazenadas no
-        // vetor grantResults, que pode estar vazio se o usuário
-        // escolheu simplesmente ignorar o pedido e não responder nada.
+
         if (requestCode == REQUEST_SEND_SMS && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
-            // Se foi positiva, podemos iniciar a SMSActivity.
+
             startSMSActivity();
         }
     }
--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/SmsActivity.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/SmsActivity.java
@@ -4,20 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.telephony.PhoneNumberUtils;
 import android.telephony.SmsManager;
 import android.widget.Button;
-import android.widget.EditText;
+
 import android.widget.Toast;
 
 import br.edu.insper.al.algumaluno.sms.R;
 
 public class SmsActivity extends AppCompatActivity {
 
-    // Método de conveniência para mostrar uma bolha de texto.
+
     private void showToast(String text) {
 
-        // Constrói uma bolha de duração curta.
         Toast toast = Toast.makeText(this, text, Toast.LENGTH_SHORT);
 
-        // Mostra essa bolha.
         toast.show();
     }
 
@@ -36,23 +34,16 @@ public class SmsActivity extends AppCompatActivity {
 
             String phone = "973715699" ;// textPhone.getText().toString();
 
-            // Esta verificação do número de telefone é bem
-            // rígida, pois exige até mesmo o código do país.
             if (!PhoneNumberUtils.isGlobalPhoneNumber(phone)) {
                 showToast("Número inválido!");
                 return;
             }
 
-            // Enviar uma mensagem de SMS. Por simplicidade,
-            // não estou verificando se foi mesmo enviada,
-            // mas é possível fazer uma versão que verifica.
             SmsManager manager = SmsManager.getDefault();
             manager.sendTextMessage(phone, null, messageSOS, null, null);
             showToast("MENSAGEM DE SOCORRO ENVIADA");
 
-            // Limpar o campo para nenhum engraçadinho
-            // ficar apertando o botão várias vezes.
-            //textPhone.setText("");
+
         });
     }
 }
